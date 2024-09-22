0551fc7bc1e32118771908fbefe2028da0873353
Mark Oates <markoates0@gmail.com>
2024-09-22 15:52:24 -0400

    * Attempt to simplify coverage algo

--- include/AlphaMesh/AlphaMesh.hpp
+++ include/AlphaMesh/AlphaMesh.hpp
@@ -18,6 +18,7 @@ namespace AlphaMesh
       AlphaMesh();
       ~AlphaMesh();
 
+      static bool area_contains_no_pixels(ALLEGRO_BITMAP* bitmap=nullptr, int x=0, int y=0, int w=0, int h=0);
       static AllegroFlare::TileMaps::TileMap<bool> process(ALLEGRO_BITMAP* bitmap=nullptr, int num_rows=16, int num_columns=16);
    };
 }
--- tests/AlphaMesh/AlphaMeshTest.cpp
+++ tests/AlphaMesh/AlphaMeshTest.cpp
@@ -23,13 +23,34 @@ TEST_F(AlphaMesh_AlphaMeshTest, can_be_created_without_blowing_up)
 TEST_F(AlphaMesh_AlphaMeshTestWithAllegroRenderingFixture,
    CAPTURE__process__will_produce_a_tile_map_of_alpha_coverage_area)
 {
+   int num_rows = 16;
+   int num_columns = 16;
    ALLEGRO_BITMAP *bitmap = get_bitmap_bin_ref()["investigator-01.png"];
-   AllegroFlare::TileMaps::TileMap<bool> coverage_tile_map = AlphaMesh::AlphaMesh::process(bitmap, 16, 16);
+   AllegroFlare::TileMaps::TileMap<bool> coverage_tile_map =
+      AlphaMesh::AlphaMesh::process(bitmap, num_rows, num_columns);
+
+   int bitmap_width = al_get_bitmap_width(bitmap);
+   int bitmap_height = al_get_bitmap_height(bitmap);
+
+   ALLEGRO_TRANSFORM camera_transform;
+   al_identity_transform(&camera_transform);
+   al_translate_transform(&camera_transform,  -bitmap_width/2, -bitmap_height/2);
+   al_scale_transform(&camera_transform,  0.5, 0.5);
+   al_translate_transform(&camera_transform,  1920/2, 1080/2);
+   al_use_transform(&camera_transform);
 
    // Draw the image
    al_draw_bitmap(bitmap, 0, 0, 0);
 
    // Draw the alpha coverage
+   int cell_width_in_pixels = bitmap_width / num_columns;
+   int cell_height_in_pixels = bitmap_height / num_rows;
+   ALLEGRO_COLOR hit = al_color_html("03a889");
+   float op = 0.3;
+   hit.r *= op;
+   hit.g *= op;
+   hit.b *= op;
+   hit.a *= op;
    for (int y=0; y<coverage_tile_map.get_num_rows(); y++)
    {
       for (int x=0; x<coverage_tile_map.get_num_columns(); x++)
@@ -37,10 +58,25 @@ TEST_F(AlphaMesh_AlphaMeshTestWithAllegroRenderingFixture,
          bool tile = coverage_tile_map.get_tile(x, y);
          if (tile)
          {
-            al_draw_filled_rectangle(x*16, y*16, x*16+16, y*16+16, al_color_html("03a889"));
+            al_draw_filled_rectangle(
+               x*cell_width_in_pixels,
+               y*cell_height_in_pixels,
+               (x+1)*cell_width_in_pixels,
+               (y+1)*cell_height_in_pixels,
+               hit
+            );
             //al_draw_filled_rectangle(x*16, y*16, x*16+16, y*16+16, al_color_html("30286c")); // Dark violet
             //al_draw_filled_rectangle(x*16, y*16, x*16+16, y*16+16, al_color_html("233732")); // Dark green
          }
+
+         al_draw_rectangle(
+            x*cell_width_in_pixels,
+            y*cell_height_in_pixels,
+            (x+1)*cell_width_in_pixels,
+            (y+1)*cell_height_in_pixels,
+            hit,
+            1.0f
+         );
       }
    }
 
