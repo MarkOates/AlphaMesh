847b221860ba5175acaee089dc2f45a3206cb5de
Mark Oates <markoates0@gmail.com>
2024-09-22 15:13:39 -0400

    * Get initial coverage algorithm working

--- src/AlphaMesh/AlphaMesh.cpp
+++ src/AlphaMesh/AlphaMesh.cpp
@@ -48,10 +48,35 @@ AllegroFlare::TileMaps::TileMap<bool> AlphaMesh::process(ALLEGRO_BITMAP* bitmap,
    result.resize(num_columns, num_rows);
    result.initialize();
 
+   int bitmap_width = al_get_bitmap_width(bitmap);
+   int bitmap_height = al_get_bitmap_height(bitmap);
+
+   int cell_width_in_pixels = bitmap_width / num_columns;
+   int cell_height_in_pixels = bitmap_height / num_rows;
+
    // HERE: process the image
-   result.set_tile(4, 6, true);
-   result.set_tile(0, 0, true);
-   result.set_tile(15, 15, true);
+   al_lock_bitmap(bitmap, ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_READONLY);
+   const float ALPHA_THRESHOLD = 0.0001f;
+
+   for (int tile_y=0; tile_y<num_rows; tile_y++)
+   {
+      for (int tile_x=0; tile_x<num_columns; tile_x++)
+      {
+         for (int pixel_y=0; pixel_y<cell_height_in_pixels; pixel_y++)
+         {
+            for (int pixel_x=0; pixel_x<cell_width_in_pixels; pixel_x++)
+            {
+               int x_pos = tile_x * cell_width_in_pixels;
+               int y_pos = tile_y * cell_height_in_pixels;
+               ALLEGRO_COLOR p = al_get_pixel(bitmap, x_pos + pixel_x, y_pos + pixel_y);
+               if (p.a <= ALPHA_THRESHOLD) result.set_tile(tile_x, tile_y, false);
+               else result.set_tile(tile_x, tile_y, true); // Is this necessary?
+            }
+         }
+      }
+   }
+
+   al_unlock_bitmap(bitmap);
 
    return result;
 }
--- tests/AlphaMesh/AlphaMeshTest.cpp
+++ tests/AlphaMesh/AlphaMeshTest.cpp
@@ -26,6 +26,10 @@ TEST_F(AlphaMesh_AlphaMeshTestWithAllegroRenderingFixture,
    ALLEGRO_BITMAP *bitmap = get_bitmap_bin_ref()["investigator-01.png"];
    AllegroFlare::TileMaps::TileMap<bool> coverage_tile_map = AlphaMesh::AlphaMesh::process(bitmap, 16, 16);
 
+   // Draw the image
+   al_draw_bitmap(bitmap, 0, 0, 0);
+
+   // Draw the alpha coverage
    for (int y=0; y<coverage_tile_map.get_num_rows(); y++)
    {
       for (int x=0; x<coverage_tile_map.get_num_columns(); x++)
@@ -33,8 +37,8 @@ TEST_F(AlphaMesh_AlphaMeshTestWithAllegroRenderingFixture,
          bool tile = coverage_tile_map.get_tile(x, y);
          if (tile)
          {
-            //al_draw_filled_rectangle(x*16, y*16, x*16+16, y*16+16, al_color_html("03a889"));
-            al_draw_filled_rectangle(x*16, y*16, x*16+16, y*16+16, al_color_html("30286c")); // Dark violet
+            al_draw_filled_rectangle(x*16, y*16, x*16+16, y*16+16, al_color_html("03a889"));
+            //al_draw_filled_rectangle(x*16, y*16, x*16+16, y*16+16, al_color_html("30286c")); // Dark violet
             //al_draw_filled_rectangle(x*16, y*16, x*16+16, y*16+16, al_color_html("233732")); // Dark green
          }
       }
